43826022af011fb41ec138342082e614a87f7401
Nitishjha7 <nitishkj5019@gmail.com>
2026-02-18 23:55:04 +0530

    generate sum of all subarray

--- ARRAY/subarray_sum.py
+++ ARRAY/subarray_sum.py
@@ -5,4 +5,9 @@ def sum_array(arr):
     for i in range(n):
         temp=0
         for j in range(i,n):
-            
+            temp +=arr[j]
+            result +=temp
+        return result
+    
+arr=[2,3,4,5]
+print(sum_array(arr))
