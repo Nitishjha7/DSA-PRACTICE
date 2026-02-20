a6149256f27a4fe4fc1a716c7e4b1c839242bda8
Nitishjha7 <nitishkj5019@gmail.com>
2026-02-20 23:11:19 +0530

    array reverse

--- ARRAY/reverse_array.py
+++ ARRAY/reverse_array.py
@@ -69,3 +69,15 @@ if __name__ == "__main__":
   
     for i in range(len(arr)):
         print(arr[i], end=" ")         
+
+#built-in method 
+# function to reverse an array
+def reverse_array(arr):
+    arr.reverse()
+
+if __name__ == "__main__":
+    arr = [1, 4, 3, 2, 6, 5]
+
+    reverse_array(arr)
+  
+    print(" ".join(map(str, arr)))
