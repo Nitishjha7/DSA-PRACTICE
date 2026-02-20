9b3686f24b74f0afd837e4334142404dcbc20d32
Nitishjha7 <nitishkj5019@gmail.com>
2026-02-20 23:05:38 +0530

    raverse array

---
+++ ARRAY/reverse_array.py
@@ -0,0 +1,24 @@
+#reverse array
+
+def reverseArray(arr):
+    n = len(arr)
+    
+    # Temporary array to store elements
+    temp = [0] * n
+  
+    # Copy elements from original array
+    # to temp in reverse order
+    for i in range(n):
+        temp[i] = arr[n - i - 1]
+  
+    # Copy elements back to original array
+    for i in range(n):
+        arr[i] = temp[i]
+
+if __name__ == "__main__":
+    arr = [1, 4, 3, 2, 6, 5]
+
+    reverseArray(arr)
+  
+    for i in range(len(arr)):
+        print(arr[i], end=" ")
